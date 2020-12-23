52a6909787247f269f7a1f37bab5c9830199c34b
Abed El Kareem Massarweh <abodi.msarwi@gmail.com>
2020-12-23 23:53:29 +0200

    moved tests

--- src/tests/DWGraph_AlgoTest.java
+++ src/tests/DWGraph_AlgoTest.java
@@ -1,7 +1,42 @@
 package tests;
 
+import api.DWGraph_Algo;
+import api.DWGraph_DS;
+import api.NodeData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DWGraph_AlgoTest {
+    static DWGraph_Algo mydwgraph;
+    static DWGraph_DS myGraph;
+    @BeforeEach
+    void build(){
+        mydwgraph= new DWGraph_Algo();
+        myGraph= new DWGraph_DS();
+        myGraph.addNode(new NodeData()); //0
+        myGraph.addNode(new NodeData()); ///1
+        myGraph.addNode(new NodeData()); //2
+        myGraph.addNode(new NodeData()); //3
+        myGraph.connect(0,1,255);
+        myGraph.connect(1,0,255);
+        myGraph.connect(1,2,100);
+        myGraph.connect(2,1,100);
+        myGraph.connect(2,3,50);
+        myGraph.connect(3,2,50);
+        myGraph.connect(3,0,25);
+        myGraph.connect(0,3,25);
+        myGraph.connect(0,2,15);
+        mydwgraph.init(myGraph);
+        //square
+    }
+    @Test
+    void test_shortestpath()
+    {
+        double distance=mydwgraph.shortestPathDist(0,1);
+        System.out.println(distance);
+        assertTrue(distance==15);
+    }
 
 }
